32a4e3c86830b85cb053ff60f048759b496af362
Anuar <anuarorozzco@gmail.com>
2024-06-14 21:21:16 -0600

    implementation of the ClientController class

--- my-first-app/src/main/java/com/anuar/my_first_app/controller/ClientControllerImpl.java
+++ my-first-app/src/main/java/com/anuar/my_first_app/controller/ClientControllerImpl.java
@@ -1,4 +1,32 @@
 package com.anuar.my_first_app.controller;
 
+import com.anuar.my_first_app.model.Client;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/client")
 public class ClientControllerImpl {
+
+    private List<Client> clients = new ArrayList<>();
+
+    @GetMapping
+    public ResponseEntity<List<Client>> getAllClients() {
+        return ResponseEntity.ok(clients);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Client> createClient(@RequestBody Client client) {
+        Long newClientId = (long) (clients.size() + 1);
+        client.setId(newClientId);
+        clients.add(client);
+        return new ResponseEntity<>(client, HttpStatus.CREATED);
+    }
+
+
+
 }
